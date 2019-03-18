b7720525fa0483d8ac54d7143f918ae88635e3c2
Colin Peters <colpeters@bethel.jw.org>
2019-03-18 09:39:13 -0400

    my changes for course

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
 @Component({
-  selector: 'mw-app',
-  templateUrl: 'app/app.component.html',
-  styleUrls: ['app/app.component.css']
+  selector:'mw-app',
+  template:'<h1>My App</h1>'
 })
-export class AppComponent { }
+
+export class AppComponent {}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,41 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
-import { CategoryListPipe } from './category-list.pipe';
-import { MediaItemFormComponent } from './media-item-form.component';
-import { MediaItemService } from './media-item.service';
-import { lookupListToken, lookupLists } from './providers';
-import { MockXHRBackend } from './mock-xhr-backend';
-import { routing } from './app.routing';
-
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    routing
+  imports:[
+    BrowserModule
   ],
-  declarations: [
-    AppComponent,
-    MediaItemComponent,
-    MediaItemListComponent,
-    FavoriteDirective,
-    CategoryListPipe,
-    MediaItemFormComponent
+  declarations:[
+    AppComponent
   ],
-  providers: [
-    MediaItemService,
-    { provide: lookupListToken, useValue: lookupLists },
-    { provide: HttpXhrBackend, useClass: MockXHRBackend }
-  ],
-  bootstrap: [
+  bootstrap:[
     AppComponent
   ]
 })
-export class AppModule {}
+export class AppModule{}
